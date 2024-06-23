19766aa0dc32a285d6d0b397283f34e5801c17f6
He Wang <wanghechn@qq.com>
2024-06-23 19:47:45 +0800

    feat: add a workflow for publishing oblogproxy-ce (#10)
    * feat: add a workflow for publishing oblogproxy-ce
    
    * update action versions

---
+++ oblogproxy-ce/start.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+
+if [[ -n ${OB_SYS_USERNAME} && -n ${OB_SYS_PASSWORD} ]]; then
+  echo "y" | /usr/local/oblogproxy/run.sh config_sys ${OB_SYS_USERNAME} ${OB_ROOT_PASSWORD}
+fi
+
+rm -rf /usr/local/oblogproxy/run/*
+/usr/local/oblogproxy/run.sh start
+exec /sbin/init
