32447108c5bcbce1cceb45b0b3450c55738b1e2a
He Wang <wanghechn@qq.com>
2024-06-25 16:46:05 +0800

    add java test case for oblogproxy-ce

--- oblogproxy-ce/start.sh
+++ oblogproxy-ce/start.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 if [[ -n ${OB_SYS_USERNAME} && -n ${OB_SYS_PASSWORD} ]]; then
-  echo "y" | /usr/local/oblogproxy/run.sh config_sys ${OB_SYS_USERNAME} ${OB_ROOT_PASSWORD}
+  echo "y" | /usr/local/oblogproxy/run.sh config_sys ${OB_SYS_USERNAME} ${OB_SYS_PASSWORD}
 fi
 
 rm -rf /usr/local/oblogproxy/run/*
