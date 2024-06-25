40966e58ef6823b0ac735e6c21dc010d7e81ae4a
chris-sun-star <hongxin.sun.vip@gmail.com>
2024-06-25 03:54:11 +0000

    add default values for ob-configserver

---
+++ ob-configserver/env.sh
@@ -0,0 +1,3 @@
+export DATABASE_TYPE=${MODE:-sqlite3}
+export SVC_ADDRESS=${SVC_ADDRESS:-$(hostname -i)}
+export SVC_PORT=${SVC_PORT:-8080}
--- ob-configserver/start.sh
+++ ob-configserver/start.sh
@@ -1,5 +1,7 @@
  #!/bin/bash
 
+source env.sh
+
 if [[ "$DATABASE_TYPE" == "mysql" ]]; then
     echo 'use mysql'
     sed -e "s/\${SVC_ADDRESS}/${SVC_ADDRESS}/" -e "s/\${SVC_PORT}/${SVC_PORT}/" -e "s/\${DATABASE_TYPE}/${DATABASE_TYPE}/" -e "s/\${META_USER}/${META_USER}/" -e "s/\${META_PASSWORD}/${META_PASSWORD}/" -e "s/\${META_HOST}/${META_HOST}/" -e "s/\${META_PORT}/${META_PORT}/" -e "s/\${META_DATABASE}/${META_DATABASE}/" ./conf/config.yaml.template | grep -v '#CONFIG_FOR_SQLITE' > ./conf/config.yaml
